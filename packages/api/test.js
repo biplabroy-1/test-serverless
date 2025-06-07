ccbbe2dfd3cd9444f31a681b030e26efd2475507
Biplab Roy <biplabr119201@gmail.com>
2025-06-08 00:45:55 +0530

    wd

--- packages/api/test.js
+++ packages/api/test.js
@@ -1,9 +1,9 @@
-import { main } from "shared/utils.js"
+// import { main } from "shared/utils.js"
 
 export function main() {
     const name = "Test Item";
-    const slug = main(name);
-    console.log(`Slug for "${name}": ${slug}`);
+    // const slug = main(name);
+    // console.log(`Slug for "${name}": ${slug}`);
     return {
       headers: {
         'Set-Cookie': 'UserID=Sammy; Max-Age=3600; Version=',
@@ -11,7 +11,7 @@ export function main() {
       },
       statusCode: 200,
       body: `<h1>Hello, ${name}!</h1>
-      <p>Your UserID is ${slug}.</p>
+      <p>Welcome to my website!</p>
       `
     }
   }
