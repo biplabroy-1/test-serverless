6db3045aa4029c515854a2ba9b19f28166a64d3c
Biplab Roy <biplabr119201@gmail.com>
2025-06-08 00:35:40 +0530

    tt

--- packages/api/test.js
+++ packages/api/test.js
@@ -1,10 +1,17 @@
+import { sukaa } from "../config/utils"
+
 export function main() {
+    const name = "Test Item";
+    const slug = sukaa(name);
+    console.log(`Slug for "${name}": ${slug}`);
     return {
       headers: {
         'Set-Cookie': 'UserID=Sammy; Max-Age=3600; Version=',
         'Content-Type': 'text/html'
       },
       statusCode: 200,
-      body: '<html><body><h3>hello</h3></body></html>'
+      body: `<h1>Hello, ${name}!</h1>
+      <p>Your UserID is ${slug}.</p>
+      `
     }
   }
---
+++ packages/config/utils.js
@@ -0,0 +1,1 @@
+export function sukaa(name) { return name.toLowerCase().replace(/ /g, '-'); };
